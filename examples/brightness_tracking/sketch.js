7c8c7a86b453a1363ed70c818223b5556d2f6b9f
eclefley <eclefley@gmail.com>
2021-12-08 14:27:59 -0800

    creating zones

--- examples/brightness_tracking/sketch.js
+++ examples/brightness_tracking/sketch.js
@@ -11,8 +11,8 @@ let LK1_mp3;
 let LK2_mp3;
 
 function preload() {
-  LK1_mp3 = loadSound("LK1.mp3");
-  LK2_mp3 = loadSound("LK2.mp3");
+  //LK1_mp3 = loadSound("LK1.mp3");
+  //LK2_mp3 = loadSound("LK2.mp3");
 }
 
 function IsDarknessDetectedZone1() {
@@ -33,12 +33,12 @@ function IsDarknessDetectedZone2() {
 
 function darknessIsDetected() {
     if (IsDarknessDetectedZone1(true)) {
-        //print("LK1");
-        LK1_mp3.play();
+        print("LK1");
+        //LK1_mp3.play();
     }
     if (IsDarknessDetectedZone2(true)) {
-        //print("LK2");
-        LK2_mp3.play();
+        print("LK2");
+        //LK2_mp3.play();
     }
 }
 
