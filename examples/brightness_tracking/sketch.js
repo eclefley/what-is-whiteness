0df17d01921b53ef2577e0a6e85010022499ff35
eclefley <eclefley@gmail.com>
2021-12-14 19:29:05 -0800

    state machine

--- examples/brightness_tracking/sketch.js
+++ examples/brightness_tracking/sketch.js
@@ -1,3 +1,5 @@
+//implement threshhold for darkest point instead of minpoint
+
 // p5.js Video capture
 let myCapture;
 // OpenCV capture helper
@@ -7,135 +9,13 @@ let myMat;
 // Mat to store the grayscale converted camera frame
 let myMatGrayscale;
 let darkestPoint;
-let LK1_mp3;
-let LK2_mp3;
-let LK3_mp3;
-let LK4_mp3;
 
-function preload() {
-  //LK1_mp3 = loadSound("LK1.mp3");
-  //LK2_mp3 = loadSound("LK2.mp3");
-  //LK3_mp3 = loadSound("LK3.mp3");
-  //LK4_mp3 = loadSound("LK4.mp3");
-}
-
-function IsDarknessDetectedZone1() {
-    if(darkestPoint.x < 100 && darkestPoint.y < 100) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function IsDarknessDetectedZone2() {
-    if(darkestPoint.x > 720 && darkestPoint.y > 540) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function IsDarknessDetectedZone3() {
-    if(darkestPoint.x > 720 && darkestPoint.y < 100) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function IsDarknessDetectedZone4() {
-    if(darkestPoint.x < 100 && darkestPoint.y > 540) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-function darknessIsDetected() {
-    if (IsDarknessDetectedZone1()) {
-        print("LK1");
-        //LK1_mp3.play();
-    }
-    if (IsDarknessDetectedZone2()) {
-        print("LK2");
-        //LK2_mp3.play();
-    }
-    if (IsDarknessDetectedZone3()) {
-        print("LK3");
-        //LK3_mp3.play();
-    }
-    if (IsDarknessDetectedZone4()) {
-        print("LK4");
-        //LK4_mp3.play();
-    }
-}
-
-function setup() {
-  createCanvas(820, 640);
-  // setup p5 capture
-  myCapture = createCapture(VIDEO);
-  myCapture.size(820, 640);
-  myCapture.hide();
-  // wait for OpenCV to init
-  p5.cv.onComplete = onOpenCVComplete;
-}
-
-function onOpenCVComplete() {
-  // create a CV capture helper
-  myCVCapture = p5.cv.getCvVideoCapture(myCapture);
-  // create a CV Mat to read new color frames into
-  myMat = p5.cv.getRGBAMat(820, 640);
-  // create a CV mat for color to grayscale conversion
-  myMatGrayscale = new cv.Mat();
-}
-
-function draw() {
-  if (p5.cv.isReady) {
-    // read from CV Capture into myMat
-    myCVCapture.read(myMat);
-    // convert Mat to grayscale
-    p5.cv.copyGray(myMat, myMatGrayscale);
-    // display Mat
-    p5.cv.drawMat(myMatGrayscale, 0, 0);
-    // get darkest point
-    darkestPoint = p5.cv.findMinLocation(myMatGrayscale);
-    // draw darkest point
-    //circle(darkestPoint.x, darkestPoint.y, 30);
-    
-      /*
-    if (IsDarknessDetected(true)) {
-        print("LK1");
-    } else {
-    image(myCapture, 0, 0);
-  }
-    */ 
-      
-  /*    
-    if(darkestPoint.x < 100 && darkestPoint.y < 100) {
-    print("LK1");
-      //if(LK1_mp3.isPlaying()) {
-        //LK1_mp3.stop();
-      //} else {
-        //LK1_mp3.play();
-      //}
-    }
-  } else {
-    image(myCapture, 0, 0);
-  }
-  */
-      darknessIsDetected();
-  }
-}
-
-
-
-/*
 let zoneOne = {
     box: {
-    x: 0,
-    y: 0,
-    width: 100,
-    height: 100,
+        x: 0,
+        y: 0,
+        width: 100,
+        height: 100,
     },
     isActive: false,
     name: 'LK1',
@@ -174,159 +54,81 @@ let zoneFour = {
     name: 'LK4',
 };
 
+let zoneArray = [zoneOne, zoneTwo, zoneThree, zoneFour];
+
 function activateZone (zone) {
     zone.isActive = true;
+    
+    print(zone.name, " was just activated");
 }
 
 function deactivateZone (zone) {
     zone.isActive = false;
+    
+        
+    print(zone.name, " was just de-activated");
+
 }
 
-
-
-
-
-
-let src;
-// p5.js Video capture
-let myCapture;
-// OpenCV capture helper
-let myCVCapture;
-// (RGBA) Mat to store the latest color camera frame
-let myMat;
-// Mat to store the grayscale converted camera frame
-let myMatGrayscale;
-let darkestPoint;
-let LK1_mp3;
-let LK2_mp3;
-let LK3_mp3;
-let LK4_mp3;
+//ex: isDarkestPointInZone(zoneOne);
+function isDarkestPointInZone(zone) {
+    if(darkestPoint.x < zone.box.width+zone.box.x && darkestPoint.y < zone.box.height+zone.box.y) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function preload() {
-//LK1_mp3 = loadSound("LK1.mp3");
-//LK2_mp3 = loadSound("LK2.mp3");
-//LK3_mp3 = loadSound("LK3.mp3");
-//LK4_mp3 = loadSound("LK4.mp3");
-}
-
-function IsDarknessDetected() {
-    if(darkestPoint.x < 100 && darkestPoint.y < 100) {
-        return true;
-    } else {
-        return false;
-    } 
-    
-    if(darkestPoint.x > 720 && darkestPoint.y > 540) {
-        return true;
-    } else {
-        return false;
-    }
-    if(darkestPoint.x > 720 && darkestPoint.y < 100) {
-        return true;
-    } else {
-        return false;
-    }
-    if(darkestPoint.x < 100 && darkestPoint.y > 540) {
-        return true;
-    } else {
-        return false;
-    }
+  //LK1_mp3 = loadSound("LK1.mp3");
+  //LK2_mp3 = loadSound("LK2.mp3");
+  //LK3_mp3 = loadSound("LK3.mp3");
+  //LK4_mp3 = loadSound("LK4.mp3");
 }
 
 function setup() {
-    createCanvas(820, 640);
-    // setup p5 capture
-    myCapture = createCapture(VIDEO);
-    myCapture.size(820, 640);
-    myCapture.hide();
-    // wait for OpenCV to init
-    p5.cv.onComplete = onOpenCVComplete;
+  createCanvas(820, 640);
+  // setup p5 capture
+  myCapture = createCapture(VIDEO);
+  myCapture.size(820, 640);
+  myCapture.hide();
+  // wait for OpenCV to init
+  p5.cv.onComplete = onOpenCVComplete;
 }
 
 function onOpenCVComplete() {
-    // create a CV capture helper
-    myCVCapture = p5.cv.getCvVideoCapture(myCapture);
-    // create a CV Mat to read new color frames into
-    myMat = p5.cv.getRGBAMat(820, 640);
-    // create a CV mat for color to grayscale conversion
-
-    myMatGrayscale = new cv.Mat();
+  // create a CV capture helper
+  myCVCapture = p5.cv.getCvVideoCapture(myCapture);
+  // create a CV Mat to read new color frames into
+  myMat = p5.cv.getRGBAMat(820, 640);
+  // create a CV mat for color to grayscale conversion
+  myMatGrayscale = new cv.Mat();
 }
 
+let i;
+let zone;
 
 function draw() {
-    if (p5.cv.isReady) {
+  if (p5.cv.isReady) {
     // read from CV Capture into myMat
-        myCVCapture.read(myMat);
-        // convert Mat to grayscale
-        p5.cv.copyGray(myMat, myMatGrayscale);
-        // display Mat
-        p5.cv.drawMat(myMatGrayscale, 0, 0);
-
-        //instead of findMinLocation for whole screen, findMinLocation for each zone ->look at lib documentation
-        darkestPoint = p5.cv.findMinLocation(myMatGrayscale);
-
-        // draw darkest point
-        circle(darkestPoint.x, darkestPoint.y, 30);
-
-/*
-if (IsDarknessDetected(true)) {
-print("LK1");
-} else {
-image(myCapture, 0, 0);
-}
-*/ 
-
-/*    
-if(darkestPoint.x < 100 && darkestPoint.y < 100) {
-print("LK1");
-//if(LK1_mp3.isPlaying()) {
-//LK1_mp3.stop();
-//} else {
-//LK1_mp3.play();
-//}
-}
-} else {
-image(myCapture, 0, 0);
-}
-*/
-/*
-        if (IsDarknessDetected()) {
-        print("LK1");
-        //LK1_mp3.play();
-        }
+    myCVCapture.read(myMat);
+    // convert Mat to grayscale
+    p5.cv.copyGray(myMat, myMatGrayscale);
+    // display Mat
+    p5.cv.drawMat(myMatGrayscale, 0, 0);
+    // get darkest point
+    darkestPoint = p5.cv.findMinLocation(myMatGrayscale);
+    // draw darkest point
+    //circle(darkestPoint.x, darkestPoint.y, 30);
+    
+    for (i = 0; i < zoneArray.length; i+=1) {
+        zone = zoneArray[i];
         
-        if (IsDarknessDetected()) {
-        print("LK2");
-        //LK1_mp3.play();
+        if(isDarkestPointInZone(zone) && zone.isActive == false) {
+            activateZone(zone);
+        } else if(!isDarkestPointInZone(zone) && zone.isActive == true) {
+            deactivateZone(zone);
         }
-        
-        if (IsDarknessDetected()) {
-        print("LK3");
-        //LK1_mp3.play();
-        }
-        
-        if (IsDarknessDetected()) {
-        print("LK4");
-        //LK1_mp3.play();
-        }
-*/
-/*
-if (IsDarknessDetectedZone2()) {
-print("LK2");
-//LK2_mp3.play();
+    } 
+  }
 }
-if (IsDarknessDetectedZone3()) {
-print("LK3");
-//LK3_mp3.play();
-}
-if (IsDarknessDetectedZone4()) {
-print("LK4");
-//LK4_mp3.play();
-}
-*/
-/*
-    }
-}
-
-*/
---
+++ sketch_unused.js
@@ -0,0 +1,223 @@
+/*
+let zoneOne = {
+    box: {
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 100,
+    },
+    isActive: false,
+    name: 'LK1',
+};
+
+let zoneTwo = {
+    box: {
+    x: 0,
+    y: 0,
+    width: 720,
+    height: 540,
+    },
+    isActive: false,
+    name: 'LK2',
+};
+
+let zoneThree = {
+    box: {
+    x: 0,
+    y: 0,
+    width: 720,
+    height: 100,
+    },
+    isActive: false,
+    name: 'LK3',
+};
+
+let zoneFour = {
+    box: {
+    x: 0,
+    y: 0,
+    width: 100,
+    height: 540,
+    },
+    isActive: false,
+    name: 'LK4',
+};
+
+function activateZone (zone) {
+    zone.isActive = true;
+}
+
+function deactivateZone (zone) {
+    zone.isActive = false;
+}
+
+
+
+
+
+
+let src;
+// p5.js Video capture
+let myCapture;
+// OpenCV capture helper
+let myCVCapture;
+// (RGBA) Mat to store the latest color camera frame
+let myMat;
+// Mat to store the grayscale converted camera frame
+let myMatGrayscale;
+let darkestPoint;
+let LK1_mp3;
+let LK2_mp3;
+let LK3_mp3;
+let LK4_mp3;
+
+function preload() {
+//LK1_mp3 = loadSound("LK1.mp3");
+//LK2_mp3 = loadSound("LK2.mp3");
+//LK3_mp3 = loadSound("LK3.mp3");
+//LK4_mp3 = loadSound("LK4.mp3");
+}
+
+function IsDarknessDetected() {
+    if(darkestPoint.x < 100 && darkestPoint.y < 100) {
+        return true;
+    } else {
+        return false;
+    } 
+    
+    if(darkestPoint.x > 720 && darkestPoint.y > 540) {
+        return true;
+    } else {
+        return false;
+    }
+    if(darkestPoint.x > 720 && darkestPoint.y < 100) {
+        return true;
+    } else {
+        return false;
+    }
+    if(darkestPoint.x < 100 && darkestPoint.y > 540) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function setup() {
+    createCanvas(820, 640);
+    // setup p5 capture
+    myCapture = createCapture(VIDEO);
+    myCapture.size(820, 640);
+    myCapture.hide();
+    // wait for OpenCV to init
+    p5.cv.onComplete = onOpenCVComplete;
+}
+
+function onOpenCVComplete() {
+    // create a CV capture helper
+    myCVCapture = p5.cv.getCvVideoCapture(myCapture);
+    // create a CV Mat to read new color frames into
+    myMat = p5.cv.getRGBAMat(820, 640);
+    // create a CV mat for color to grayscale conversion
+
+    myMatGrayscale = new cv.Mat();
+}
+
+
+function draw() {
+    if (p5.cv.isReady) {
+    // read from CV Capture into myMat
+        myCVCapture.read(myMat);
+        // convert Mat to grayscale
+        p5.cv.copyGray(myMat, myMatGrayscale);
+        // display Mat
+        p5.cv.drawMat(myMatGrayscale, 0, 0);
+
+        //instead of findMinLocation for whole screen, findMinLocation for each zone ->look at lib documentation
+        darkestPoint = p5.cv.findMinLocation(myMatGrayscale);
+
+        // draw darkest point
+        circle(darkestPoint.x, darkestPoint.y, 30);
+
+/*
+if (IsDarknessDetected(true)) {
+print("LK1");
+} else {
+image(myCapture, 0, 0);
+}
+*/ 
+
+/*    
+if(darkestPoint.x < 100 && darkestPoint.y < 100) {
+print("LK1");
+//if(LK1_mp3.isPlaying()) {
+//LK1_mp3.stop();
+//} else {
+//LK1_mp3.play();
+//}
+}
+} else {
+image(myCapture, 0, 0);
+}
+*/
+/*
+        if (IsDarknessDetected()) {
+        print("LK1");
+        //LK1_mp3.play();
+        }
+        
+        if (IsDarknessDetected()) {
+        print("LK2");
+        //LK1_mp3.play();
+        }
+        
+        if (IsDarknessDetected()) {
+        print("LK3");
+        //LK1_mp3.play();
+        }
+        
+        if (IsDarknessDetected()) {
+        print("LK4");
+        //LK1_mp3.play();
+        }
+*/
+/*
+if (IsDarknessDetectedZone2()) {
+print("LK2");
+//LK2_mp3.play();
+}
+if (IsDarknessDetectedZone3()) {
+print("LK3");
+//LK3_mp3.play();
+}
+if (IsDarknessDetectedZone4()) {
+print("LK4");
+//LK4_mp3.play();
+}
+*/
+/*
+    }
+}
+
+*/
+
+     /*
+    if (IsDarknessDetected(true)) {
+        print("LK1");
+    } else {
+    image(myCapture, 0, 0);
+  }
+    */ 
+      
+  /*    
+    if(darkestPoint.x < 100 && darkestPoint.y < 100) {
+    print("LK1");
+      //if(LK1_mp3.isPlaying()) {
+        //LK1_mp3.stop();
+      //} else {
+        //LK1_mp3.play();
+      //}
+    }
+  } else {
+    image(myCapture, 0, 0);
+  }
+  */
